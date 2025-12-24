f8669464314cc1a69d5e45c49aad021fe3f3494b
mambasa <101706131+kal990574@users.noreply.github.com>
2025-12-24 10:55:49 +0900

    feat: obect pooling base script 구현

---
+++ Assets/02.Scripts/Core/ObjectPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using IPoolable = _02.Scripts.Character.Interfaces.IPoolable;
+
+namespace _02.Scripts.Core
+{
+    public class ObjectPool<T> : MonoBehaviour where T : MonoBehaviour, IPoolable
+    {
+        [Header("Pool Settings")] 
+        [SerializeField] private T _prefab;
+        [SerializeField] private int _initialSize = 10;
+        [SerializeField] private Transform _poolParent;
+
+        private Queue<T> _pool = new Queue<T>();
+        public int PooledCount => _pool.Count;
+        private void Awake()
+        {
+            Prewarm(_initialSize);
+        }
+
+        public void Prewarm(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                T obj = CreateNew();
+                obj.gameObject.SetActive(false);
+                _pool.Enqueue(obj);
+            }
+        }
+
+        public T Get()
+        {
+            T obj = _pool.Count > 0 ? _pool.Dequeue() : CreateNew();
+            obj.gameObject.SetActive(true);
+            obj.OnSpawn();
+            return obj;
+        }
+
+        public void Return(T obj)
+        {
+            obj.OnDespawn();
+            obj.gameObject.SetActive(false);
+            _pool.Enqueue(obj);
+        }
+
+        private T CreateNew()
+        {
+            Transform parent = _poolParent != null ? _poolParent : transform;
+            T obj = Instantiate(_prefab, parent);
+            return obj;
+        }
+    }
+}
